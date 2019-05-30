fb3ee75a4910e78af8d1e85205f4e491a7326547
xiaotuan <qin_ty@yuntianzn.com>
2019-05-30 11:04:00 +0800

    添加第4天=>SuperGuess5->超级猜图（答案完毕以后弹出提示对话框）

---
+++ Day04/SuperGuess5/SuperGuess5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperGuess5
+//
+//  Created by QinTuanye on 2019/5/30.
+//  Copyright © 2019 QinTuanye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
